d3cdb72b7ba1e0ebac4d4dc7c207a1571c57e319
David Ludwig <dludwig@pobox.com>
2016-10-01 12:54:22 -0400

    added premake5 file to help create IDE/debugger-friendly workspaces; added SDL2 2.0.4 source to help build the test program; make git ignore line-endings (such as when adding dependencies)

---
+++ test/premake5.lua
@@ -0,0 +1,22 @@
+workspace "test-DLRaster"
+    configurations { "Debug", "Release" }
+
+project "test"
+    kind "ConsoleApp"
+    language "C++"
+
+    files { "*.h", "*.cpp" }
+    includedirs { "external/SDL2/include" }
+
+    filter { "system:macosx" }
+        buildoptions { "-std=c++11" }
+        frameworkdirs { "external" }
+        linkoptions { "-framework SDL2", "-framework OpenGL", "-rpath @executable_path/../../external" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
