9caf1f7929aa63aabb87977b00f769daa57b8f93
David Ludwig <dludwig@pobox.com>
2016-10-23 14:29:10 -0400

    use premake5 to generate MSVC project file(s) for test app

--- test/premake5.lua
+++ test/premake5.lua
@@ -1,6 +1,12 @@
 workspace "test-DLRaster"
     configurations { "Debug", "Release" }
 
+externalproject "SDL"
+    location "external/SDL2/VisualC/SDL"
+    uuid "81CE8DAF-EBB2-4761-8E45-B71ABCCA8C68"
+    language "C"
+    kind "SharedLib"
+
 project "test"
     kind "ConsoleApp"
     language "C++"
@@ -12,6 +18,10 @@ project "test"
         buildoptions { "-std=c++11" }
         frameworkdirs { "external" }
         linkoptions { "-framework SDL2", "-framework OpenGL", "-rpath @executable_path/../../external" }
+
+    filter { "system:windows" }
+        links { "SDL" }
+        postbuildcommands { "copy /Y Win32\\%{cfg.buildcfg}\\SDL2.dll bin\\%{cfg.buildcfg}\\" }
 
     filter "configurations:Debug"
         defines { "DEBUG" }
